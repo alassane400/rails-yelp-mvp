684e005750b5aa6b0be1d8cf3e4f1c6d0c24c228
Alassane Kabore <alassanekat@gmail.com>
2022-11-17 16:09:40 +0100

    rails new

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+kfc = Restaurant.new(name: "kfc",address:"lille", phone_number:"07234567", category:"french" )
+kfc.save
+
+mcdo = Restaurant.new(name: "mcdo",address:"arras", phone_number:"03234967", category:"belgian" )
+mcdo.save
+
+grill = Restaurant.new(name: "grill",address:"paris", phone_number:"0328867", category:"italian" )
+grill.save
+
+sushi = Restaurant.new(name: "sushi",address:"reims", phone_number:"0397547", category:"japanese" )
+sushi.save
+
+
+boile = Restaurant.new(name: "boile",address:"nantes", phone_number:"0142567", category:"french" )
+boile.save
+
